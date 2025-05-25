f6dbac5cf481aaa7628d18d40dcf5dad4e61f6a9
leon nwosu <nwosuleon@gmail.com>
2025-05-24 22:06:33 -0500

    normalized ready for training

--- model/model.py
+++ model/model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 import os
 
 
@@ -53,15 +54,42 @@ def main():
     # Split the data
     X_train, X_test, y_train, y_test = split_data(df)
     
+   
+    # normalize the train and test ammount ans time columndata that does not have the class label
+
+    # Normalize the 'Amount' and 'Time' columns
+
+    if not {'Time','Amount'}.issubset(X_test.columns):
+        raise KeyError('Amount and TIme column is required in the dataset')
+    
+
+    scaler = MinMaxScaler()
+    X_train_scaled_cols = scaler.fit_transform(X_train[['Time','Amount']])
+    X_test_scaled_cols = scaler.transform(X_test[['Time','Amount']])
+
+    X_train[['Time','Amount']] = X_train_scaled_cols
+    X_test[['Time','Amount']] = X_test_scaled_cols
+
+
     # Save the split data to CSV files
-    # X_train.to_csv('X_train.csv', index=False)
-    # X_test.to_csv('X_test.csv', index=False)
-    # y_train.to_csv('y_train.csv', index=False)
-    # y_test.to_csv('y_test.csv', index=False)
+    X_train.to_csv('X_train.csv', index=False)
+    X_test.to_csv('X_test.csv', index=False)
+    y_train.to_csv('y_train.csv', index=False)
+    y_test.to_csv('y_test.csv', index=False)
+
+
+
+
+
+
+
+
+
 
     # train the model
     # model = train_model(X_train, y_train)
     # save the model
+
 
 
 
